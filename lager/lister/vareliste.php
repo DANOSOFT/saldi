b44b7b89e6e5a6a42bebc28c84e8fc8c1a46aae8
Patrick Madsen <patrickmadsen23@gmail.com>
2025-05-26 12:22:52 +0300

    fixed a bug when lukket was equals to nothing it wouldent show the item

--- lager/lister/vareliste.php
+++ lager/lister/vareliste.php
@@ -292,7 +292,7 @@ $filters[] = array(
             "name" => "Vis udgået",
             "checked" => "checked",
             "sqlOn" => "",
-            "sqlOff" => "(v.lukket IS NULL OR v.lukket = '0')",
+            "sqlOff" => "(v.lukket IS NULL OR v.lukket = '0' or v.lukket)",
         )
     )
 );
