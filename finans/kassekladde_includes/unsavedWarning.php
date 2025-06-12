b7e5c3d1a0137271e6fa4681aa7ba76d5ca0d0c2
sawaneh1 <sawaneh.itg@gmail.com>
2025-06-12 00:18:57 +0000

    prompt you for an unsaved chnages

---
+++ finans/kassekladde_includes/unsavedWarning.php
@@ -0,0 +1,9 @@
+<div id="unsavedModal" style="display:none; position:fixed; left:0; top:0; width:100vw; height:100vh; background:rgba(0,0,0,0.3); z-index:9999; align-items:center; justify-content:center;">
+  <div style="background:#fff; padding:2em; border-radius:8px; max-width:350px; margin:auto; box-shadow:0 2px 16px #0003; text-align:center;">
+    <div style="margin-bottom:1em;">You have unsaved changes.<br>Do you want to save before sorting?</div>
+    <button id="modalSaveBtn" style="margin-right:1em;">Save</button>
+    <button id="modalCancelBtn">Cancel</button>
+  </div>
+</div>
+</body>
+</html>
