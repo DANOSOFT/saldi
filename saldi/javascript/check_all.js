dd876d2abd9f66e30aabe6114b011c213ad7333e
Loe4g <loeurdo@hotmail.com>
2025-06-20 13:04:09 +0100

    [import] Final source tree for v3.6 release

--- saldi/javascript/check_all.js
+++
@@ -1,7 +0,0 @@
-/* This script and many more are available free online at
-The JavaScript Source!! http://javascript.internet.com
-Created by: Hillel Aftel |  */
-function checkAll(checkname, exby) {
-  for (i = 0; i < checkname.length; i++)
-  checkname[i].checked = exby.checked? true:false
-}
