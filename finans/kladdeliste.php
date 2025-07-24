5f76f30e37e8b4b291e9c975e08ead0eeac1fd37
Patrick Madsen <patrickmadsen23@gmail.com>
2025-07-24 13:00:49 +0300

    fixed design bug

--- finans/kladdeliste.php
+++ finans/kladdeliste.php
@@ -35,7 +35,7 @@ $modulnr=2;
 $title="kladdeliste";	
 $backUrl = isset($_GET['returside'])
 ? $_GET['returside']
-: 'javascript:window.history.go(-2);';
+: '../index/menu.php';
 include("../includes/connect.php");
 include("../includes/std_func.php");
 $query = db_select("SELECT * FROM settings WHERE var_name = 'apiKey' AND var_grp = 'easyUBL'", __FILE__ . " linje " . __LINE__);
@@ -81,7 +81,7 @@ if ($menu=='T') {
 
 	print "<td width='10%'  title='".findtekst('1599|Klik her for at lukke kladdelisten', $sprog_id)."'>"; #20210721
 	print "<a href='$backUrl' accesskey='L'><button style='$buttonStyle; width:100%' onMouseOver=\"this.style.cursor = 'pointer'\">".findtekst('30|Tilbage', $sprog_id)."</button></a></td>";
-	print "<td width=70% style=$topStyle align=center>".findtekst('639|Kladdeliste', $sprog_id)."</td>";
+	print "<td width=70% style='$topStyle' align=center>".findtekst('639|Kladdeliste', $sprog_id)."</td>";
 	print "<td id='tutorial-help' width=5% style=$buttonStyle>
 	<button class='center-btn' style='$buttonStyle; width:100%' onMouseOver=\"this.style.cursor='pointer'\">
 		".findtekst('2564|Hjælp', $sprog_id)."
