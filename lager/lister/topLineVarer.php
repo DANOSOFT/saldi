7771df5d83958bc26e589fd7ab0096ced3a98370
Sor1s1 <mmsor1s1@hotmail.com>
2025-08-14 16:55:46 +0300

    Translations/corrections + added texts to 'findtekst'

--- lager/lister/topLineVarer.php
+++ lager/lister/topLineVarer.php
@@ -120,8 +120,8 @@
 		print "<td id='create-new' width=5% style=$buttonStyle>
 			<a href=../varekort.php accesskey='L'>
 			<button class='center-btn' style='$buttonStyle; width:100%' onMouseOver=\"this.style.cursor='pointer'\">
-				$add_icon
-				Ny  
+				$add_icon".
+				findtekst('39|Ny', $sprog_id)." 
 			</button></a></td>";
 	}
 
