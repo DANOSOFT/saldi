548991c58d2717bbf4f46a6038f50281851a2579
Patrick Madsen <patrickmadsen23@gmail.com>
2025-08-28 13:05:14 +0300

    fixed styling

--- debitor/ordre.php
+++ debitor/ordre.php
@@ -3736,7 +3736,7 @@ print "<td align='center' class='tableHeader'><b>".findtekst('428|Rabat', $sprog
 		if ($bynavn==$k_bynavn) $tekstcolor="#444444";
 		else {$tekstcolor="#ff0000";$ret=1;};
 		$txt46 = findtekst('46|By', $sprog_id);
-		print "<span style=\"color:$tekstcolor;\" title=\"$k_bynavn\">$txt46</span></td><td colspan=\"2\"><input class = 'inputbox' type = 'text' style=\"width:45px;\" name=\"postnr\" onfocus=\"document.forms[0].fokus.value=this.name;\"  value=\"$postnr\" onchange=\"javascript:docChange = true;\" $disabled><input class = 'inputbox' type = 'text' style=\"width:150px;margin-left:3px;\" name=\"bynavn\" onfocus=\"document.forms[0].fokus.value=this.name;\" value=\"$bynavn\" onchange=\"javascript:docChange = true;\" $disabled></td></tr>\n";
+		print "<span style=\"color:$tekstcolor;\" title=\"$k_bynavn\">$txt46</span></td><td colspan=\"2\"><input class = 'inputbox' type = 'text' style=\"width:45px;\" name=\"postnr\" onfocus=\"document.forms[0].fokus.value=this.name;\"  value=\"$postnr\" onchange=\"javascript:docChange = true;\" $disabled><input class = 'inputbox' type = 'text' style=\"width:152px;margin-left:3px;\" name=\"bynavn\" onfocus=\"document.forms[0].fokus.value=this.name;\" value=\"$bynavn\" onchange=\"javascript:docChange = true;\" $disabled></td></tr>\n";
 		if ($land==$k_land) $tekstcolor="#444444";
 		else {$tekstcolor="#ff0000";$ret=1;};
 		print "<tr><td style=\"color:$tekstcolor;\" title=\"$k_land\">".findtekst('593|Lande', $sprog_id)."</td><td colspan=\"2\"><input class = 'inputbox' type = 'text' style=\"width:200px\" name=\"land\" onfocus=\"document.forms[0].fokus.value=this.name;\"  value=\"$land\" onchange=\"javascript:docChange = true;\" $disabled></td></tr>\n";
@@ -5462,7 +5462,7 @@ print "<td valign='top'><input class='inputbox' type='text' style='text-align:ri
 	if (!$saetnr) {
     $txt2130 = findtekst('2130|Slet ordrelinje', $sprog_id);
     print "<td valign = 'top' align='right' title='$txt2130'>";
-    print "<button type='button' style='color:red;' ";
+    print "<button type='button' style='background: red; color: #fff;' ";
     print "onclick=\"if (confirm('Slet linje $x?')) { document.getElementsByName('posn$x')[1].value='-'; ";
     print "document.getElementsByName('ordre')[0].submit.click(); }\">X</button></td>\n";
 	} else print "<td></td>";
