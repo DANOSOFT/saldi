af5b5092c91550bdaf3ea4a96d033470dd552d15
PeterRude <peter.rude@gmail.com>
2025-09-08 16:47:01 +0300

    Just a little cleanup

--- debitor/payments/print_receipt.php
+++ debitor/payments/print_receipt.php
@@ -29,19 +29,22 @@ if (isset($_GET['filename'])) $filename = $_GET['filename'];
 if (!$kasse) $kasse = $_COOKIE['saldi_pos'];
 
 if (!$printserver) {
-  $qtxt="select box3,box4,box5,box6,box11,box12 from grupper where art = 'POS' and kodenr='2'";
-  $r = db_fetch_array(db_select($qtxt,__FILE__ . " linje " . __LINE__));
-  $x=$kasse-1;
-  $tmp=explode(chr(9),$r['box3']);
-  $printserver=trim($tmp[$x]);
-  if (!$printserver)$printserver='localhost';
-  if ($printserver=='box' || $printserver=='saldibox') {
-    $filnavn="http://saldi.dk/kasse/".$_SERVER['REMOTE_ADDR'].".ip";
-    $printserver=file_get_contents($filnavn);
+  $r = db_fetch_array(db_select("select box3 from grupper where art = 'POS' and kodenr='2' and fiscal_year = '$regnaar'", __FILE__ . " linje " . __LINE__));
+  $x = $kasse - 1;
+  $tmp = explode(chr(9), $r['box3']);
+  $printserver = trim($tmp[$x]);
+  if (!$printserver) {
+    $printserver = 'localhost';
   }
 }
-#$kasse = 1;
-if (!$printserver) $printserver = "localhost";
+if ($printserver == 'box' || $printserver == 'saldibox') {
+  $filnavn = "http://saldi.dk/kasse/" . $_SERVER['REMOTE_ADDR'] . ".ip";
+  if ($fp = fopen($filnavn, 'r')) {
+    $printserver = trim(fgets($fp));
+    fclose($fp);
+  }
+}
+
 $bon = '';
 if ($type == 'flatpay') {
   $data=explode('[',file_get_contents($filename));
