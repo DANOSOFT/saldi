1e670494523ea964eda59c0b95926adb4e3fd760
Sor1s1 <mmsor1s1@hotmail.com>
2025-10-28 16:45:08 +0200

    Translations/corrections

--- debitor/betalinger_settings.php
+++ debitor/betalinger_settings.php
@@ -22,7 +22,7 @@
 $s_id = session_id();
 
 $modulnr = 12;
-$title = "Betalingsdato indstillinger";
+#$title = "Betalingsdato indstillinger";
 $css = "../css/standard.css";
 
 global $menu;
@@ -30,6 +30,9 @@ global $menu;
 include("../includes/connect.php");
 include("../includes/online.php");
 include("../includes/std_func.php");
+include("../includes/topline_settings.php");
+
+$title = findtekst('2732|Indstillinger for betalingsdato', $sprog_id);
 
 $save = isset($_POST['save']) ? $_POST['save'] : null;
 $paymentDays = isset($_POST['paymentDays']) ? intval($_POST['paymentDays']) : 1;
@@ -74,13 +77,27 @@ if ($menu == 'T') {
     include_once '../includes/topmenu/header.php';
     print "<div class='$kund'>$title</div>
     <div class='content-noside'>";
+} elseif ($menu == 'S') {
+    print "<table width='100%' height='100%' border='0' cellspacing='0' cellpadding='0'><tbody>";
+    print "<tr><td height = '25' align='center' valign='top'>";
+    print "<table width='100%' align='center' border='0' cellspacing='2' cellpadding='0'><tbody>";
+
+    print "<td width='10%'><a href='betalingsliste.php' accesskey=L>";
+    print "<button style='$buttonStyle; width:100%' onMouseOver=\"this.style.cursor = 'pointer'\">".findtekst('2172|Luk', $sprog_id)."</button></a></td>";
+
+    print "<td width='80%' style='$topStyle' align='center'>$title</td>";
+
+    print "<td width='10%' style='$topStyle'></td>";
+    print "</tbody></table>";
+    print "</td></tr>";
+    print "<tr><td valign='top'>";
 } else {
     print "<table width='100%' height='100%' border='0' cellspacing='0' cellpadding='0'><tbody>";
     print "<tr><td height = '25' align='center' valign='top'>";
     print "<table width='100%' align='center' border='0' cellspacing='2' cellpadding='0'><tbody>";
-    print "<td width='10%' $top_bund><font face='Helvetica, Arial, sans-serif' color='#000066'><a href='betalingsliste.php' accesskey=L>Luk</a></td>";
+    print "<td width='10%' $top_bund><font face='Helvetica, Arial, sans-serif' color='#000066'><a href='betalingsliste.php' accesskey=L>".findtekst('2172|Luk', $sprog_id)."</a></td>";
     print "<td width='80%' $top_bund><font face='Helvetica, Arial, sans-serif' color='#000066'>$title</td>";
-    print "<td width='10%' $top_bund><font face='Helvetica, Arial, sans-serif' color='#000066'><a href='betalingsliste.php' accesskey=L>Luk</a></td>";
+    print "<td width='10%' $top_bund></td>";
     print "</tbody></table>";
     print "</td></tr>";
     print "<tr><td valign='top'>";
@@ -88,30 +105,41 @@ if ($menu == 'T') {
 
 if (isset($_GET['saved'])) {
     print "<div style='background-color: #d4edda; color: #155724; padding: 10px; margin: 10px; border: 1px solid #c3e6cb; border-radius: 4px;'>";
-    print "Indstillinger er gemt!";
+    print findtekst('2731|Indstillingerne er gemt', $sprog_id)."!";
     print "</div>";
 }
+
+$txt1  = findtekst('2732|Indstillinger for betalingsdato', $sprog_id);
+$txt2  = findtekst('2733|Standard betalingsfrist (dage)', $sprog_id);
+$txt3  = findtekst('914|Beskrivelse', $sprog_id);
+$txt4  = findtekst('2734|Angiv hvor mange dage der som standard skal gå fra dags dato til betalingsdatoen', $sprog_id);
+$txt5  = findtekst('3|Gem', $sprog_id);
+$txt6  = findtekst('2737|Aktuelle', $sprog_id);
+$txt7  = lcfirst(findtekst('122|Indstillinger', $sprog_id));
+$txt8  = findtekst('2735|Dags dato', $sprog_id);
+$txt9  = findtekst('2736|Betalingsdato', $sprog_id);
+$txt10 = findtekst('5|Annullér', $sprog_id);
 
 print "<form method='post' action='betalinger_settings.php'>";
 print "<table cellpadding='5' cellspacing='5' border='0' width='100%'>";
 
 print "<tr>";
-print "<td colspan='2'><h3>Betalingsdato indstillinger</h3></td>";
+print "<td colspan='2'><h3>$txt1</h3></td>";
 print "</tr>";
 
 print "<tr>";
-print "<td width='30%'><b>Antal dage fra i dag:</b></td>";
+print "<td width='30%'><b>$txt2:</b></td>";
 print "<td><input type='number' name='paymentDays' value='$currentPaymentDays' min='0' max='365' style='width: 80px;'></td>";
 print "</tr>";
 
 print "<tr>";
-print "<td><b>Forklaring:</b></td>";
-print "<td>Angiv hvor mange dage fra i dag betalingsdatoen skal være som standard.</td>";
+print "<td><b>$txt3:</b></td>";
+print "<td>$txt4.</td>";
 print "</tr>";
 
 print "<tr>";
-print "<td><b>Eksempel:</b></td>";
-print "<td>I dag: <strong>$today</strong> → Betalingsdato: <strong>$exampleDate</strong></td>";
+print "<td><b>$txt6 $txt7:</b></td>";
+print "<td>$txt8: <strong>$today</strong> → $txt9: <strong>$exampleDate</strong></td>";
 print "</tr>";
 
 print "<tr>";
@@ -120,9 +148,9 @@ print "</tr>";
 
 print "<tr>";
 print "<td colspan='2'>";
-print "<input type='submit' name='save' value='Gem indstillinger' style='background-color: #007bff; color: white; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;'>";
+print "<input type='submit' name='save' value='$txt5 $txt7' style='background-color: #007bff; color: white; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;'>";
 print "&nbsp;&nbsp;";
-print "<input type='button' value='Annuller' onclick=\"location.href='betalingsliste.php'\" style='background-color: #6c757d; color: white; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;'>";
+print "<input type='button' value='$txt10' onclick=\"location.href='betalingsliste.php'\" style='background-color: #6c757d; color: white; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;'>";
 print "</td>";
 print "</tr>";
 
