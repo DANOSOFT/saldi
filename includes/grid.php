0101e1e067d19a60d80307180ceeb05fc31918c9
sawaneh1 <sawaneh.itg@gmail.com>
2026-02-16 10:37:59 +0000

    fix searching not working after paginating

--- includes/grid.php
+++ includes/grid.php
@@ -1004,7 +1004,7 @@ function render_table_headers($columns, $searchTerms, $totalWidth, $id, $metaCol
                             <div class="dropdown">
                                 <svg id="turn-arrow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="34px" fill="#000000"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
                                 <div class="dropdown-content">
-                                    <button type="submit" class="dropdown-btn">
+                                    <button type="submit" class="dropdown-btn" onclick="document.getElementsByName('offset[$id]')[0].value='0';">
                                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
                                         {$txt1}
                                     </button>
@@ -2098,6 +2098,19 @@ function render_dropdown_script($id, $query) {
             if (scrollpos && element) {
                 element.scrollTo(0, parseInt(scrollpos, 10));
             }
+            
+            // Reset offset to 0 when pressing Enter in search fields
+            var searchFields = document.querySelectorAll('input[name^="search[$id]"]');
+            searchFields.forEach(function(field) {
+                field.addEventListener('keypress', function(e) {
+                    if (e.key === 'Enter') {
+                        var offsetField = document.getElementsByName('offset[$id]')[0];
+                        if (offsetField) {
+                            offsetField.value = '0';
+                        }
+                    }
+                });
+            });
         });
 
 function saveScrollPosition() {
