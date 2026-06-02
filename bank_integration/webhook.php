8b9885655b88d20cb35d5f40826eb2e6590f3759
sirrolin <nicolaieno@msn.com>
2026-06-02 12:25:57 +0300

    Bank integration implementation

--- bank_integration/webhook.php
+++ bank_integration/webhook.php
@@ -26,8 +26,10 @@
 //
 // 20260521 NTR - Initial version. Handles OAuth callback from Aiia (Nordic API Gateway).
 
+/*
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+*/
 
 @session_start();
 $s_id = session_id();
