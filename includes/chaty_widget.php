2bb495e3b915b13e08192346eb4d3c78d9620ebf
Test <test@example.com>
2026-06-27 13:17:25 +0200

    Integrate Chaty chatbot widget

---
+++ includes/chaty_widget.php
@@ -0,0 +1,8 @@
+<script
+    async
+    src="https://wuweiworkai.com/chaty-v2/widget.js"
+    data-widget-id="saldi-prod"
+    data-brand="SALDI.dk"
+    data-lang="da"
+    data-theme-color="#2872fa">
+</script>
