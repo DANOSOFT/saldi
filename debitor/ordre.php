aa7cc869aa6ec397a936fbc6b1856d887e5d81d5
sawaneh1 <sawaneh.itg@gmail.com>
2026-09-10 16:05:21 +0000

    Address review: sanitise every returside boundary, require literal popup=1, keep returside on the print-selection page
    ordre.php: the POST returside on save, the GET fallback on the delete path
    and the returside stored on the order row now pass nav_sanitize_returside();
    the popup request flag must be exactly '1'.
    udskriftsvalg.php: the quick-invoice redirect, the Luk link and the
    Plukliste/Ordrebekraeftelse/Foelgeseddel links carry returside, and Luk
    honours it when set.

--- debitor/ordre.php
+++ debitor/ordre.php
@@ -113,7 +113,7 @@
 // 20260909 Sawaneh JOB-124: partial-delivery packing-slip buttons pass a returside back to the order.
 // 20260910 CL/NTR SST-763: tekst ids 5170-5190 moved to 3385-3404; 5180 replaced by existing 828 (Fakturanr.).
 // 20260910 Sawaneh Back button: luk.php returside only on the popup=1 request flag (was the popup
-//                  preference, which sent inline/iframe users to the login page); GET returside sanitised.
+//                  preference, which sent inline/iframe users to the login page); GET, POST and stored returside sanitised.
 
 @session_start();
 $s_id = session_id();
@@ -422,7 +422,7 @@ if (isset($sag_id)) { // Returside sættes til 'sager' fra sager.php #20210715
 // luk.php only when THIS window is a popup (popup=1 flag), not on the user's popup
 // preference: inside the new-design iframe luk.php cannot close anything and
 // used to end on the login page.
-$isPopupRequest = !empty($_GET['popup']) || !empty($_POST['popup']);
+$isPopupRequest = ifset($_GET, 'popup') === '1' || ifset($_POST, 'popup') === '1';
 if ($isPopupRequest) $returside = "../includes/luk.php?id=$id&tabel=ordrer";
 
 if (($ret_tekst = if_isset($_GET, NULL, 'ret_tekst')) && ($id = if_isset($_GET, NULL, 'id'))) tekstopslag($sort, $id);
@@ -1141,7 +1141,7 @@ if ($b_submit) {
 	$datotid         = if_isset($_POST, NULL, 'datotid');
 	$nr              = if_isset($_POST, NULL, 'nr');
 
-	$returside = if_isset($_POST, NULL, 'returside');
+	$returside = nav_sanitize_returside(ifset($_POST, 'returside'));
 	if ($status < 3) $status = if_isset($_POST, NULL, 'status');
 	$godkend = if_isset($_POST, NULL, 'godkend');
 	$restordre = if_isset($_POST, NULL, 'restordre');
@@ -1607,7 +1607,7 @@ if (($status < 3 || strstr($b_submit, "Kopi") || strstr($b_submit, "Kred")) && $
 		if ($sag_id) { #20140507-2
 			header("location:../sager/sager.php?funktion=vis_sag&sag_id=$sag_id");
 		} else {
-			if (!$returside) $returside = if_isset($_GET['returside']);
+			if (!$returside) $returside = nav_sanitize_returside(ifset($_GET, 'returside'));
 			print "<meta http-equiv=\"refresh\" content=\"0;URL=$returside\">\n";
 			exit;
 		}
@@ -3664,7 +3664,7 @@ function ordreside($id, $regnskab)
 		$datotid = if_isset($row, '', 'datotid');
 		$nr = if_isset($row, 0, 'nr') * 1;
 		if (!$returside && if_isset($row, false, 'returside')) {
-			$returside = $row['returside'];
+			$returside = nav_sanitize_returside($row['returside']);
 		}
 		$omkunde = if_isset($row, '', 'omvbet') ? 'on' : '';
 		$betalt = if_isset($row, 0, 'betalt');
--- debitor/udskriftsvalg.php
+++ debitor/udskriftsvalg.php
@@ -52,6 +52,7 @@ $returQuery=$returside ? '&returside=' . urlencode($returside) : '';
 
 if ($valg=="tilbage" || $udskriv_til=='ingen') {
 	if ($popup) print "<meta http-equiv=\"refresh\" content=\"0;URL=../includes/luk.php\">";
+	elseif ($returside) print "<meta http-equiv=\"refresh\" content=\"0;URL=" . htmlspecialchars($returside, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . "\">";
 	else print "<meta http-equiv=\"refresh\" content=\"0;URL=ordre.php?tjek=$id&id=$id\">";
 	exit;
 }
@@ -76,14 +77,14 @@ if ($valg) {
 
 if (db_fetch_array(db_select("select id from grupper where art = 'DIV' and kodenr = '3' and box4='on'",__FILE__ . " linje " . __LINE__))) {
 #	$hurtigfakt='on';
-	print "<meta http-equiv=refresh content=0;url='udskriftsvalg.php?id=$id&valg=-1&formular=2'>";
+	print "<meta http-equiv=refresh content=0;url='udskriftsvalg.php?id=$id&valg=-1&formular=2$returQuery'>";
 	exit;
 }
 
 print "<table width=\"100%\" height=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tbody>";
 print "<tr><td align=\"center\" valign=\"top\">";
 print "<table width=\"100%\" height=\"1%\" align=\"center\" border=\"0\" cellspacing=\"2\" cellpadding=\"0\"><tbody>";
-print "<td width=\"10%\" $top_bund><a href=udskriftsvalg.php?valg=tilbage&id=$id accesskey=L>Luk</a></td>";
+print "<td width=\"10%\" $top_bund><a href=\"udskriftsvalg.php?valg=tilbage&id=$id$returQuery\" accesskey=L>Luk</a></td>";
 print "<td width=\"80%\" $top_bund align=\"center\">Udskriftsvalg</td>";
 print "<td width=\"10%\" $top_bund align = \"right\"><br></td>";
 print "</tbody></table>";
@@ -100,10 +101,10 @@ $q = db_select("select * from batch_salg where ordre_id = $id",__FILE__ . " linj
 while ($r = db_fetch_array($q)) {
 	if ($r['lev_nr']>$lev_nr) {$lev_nr=$r['lev_nr'];}
 }
-if ($leveres) print "<tr><td align=center> <a href='udskriftsvalg.php?id=$id&valg=-1&formular=9'>Plukliste</a></td></tr>";
-print "<tr><td align=center> <a href='udskriftsvalg.php?id=$id&valg=-1&formular=2'>Ordrebekr&aelig;ftelse</a></td></tr>";
+if ($leveres) print "<tr><td align=center> <a href='udskriftsvalg.php?id=$id&valg=-1&formular=9$returQuery'>Plukliste</a></td></tr>";
+print "<tr><td align=center> <a href='udskriftsvalg.php?id=$id&valg=-1&formular=2$returQuery'>Ordrebekr&aelig;ftelse</a></td></tr>";
 for ($x=1; $x<=$lev_nr; $x++) {
-	print "<tr><td align=center> <a href='udskriftsvalg.php?id=$id&valg=$x&formular=3'>F&oslash;lgeseddel $x</a></td></tr>";
+	print "<tr><td align=center> <a href='udskriftsvalg.php?id=$id&valg=$x&formular=3$returQuery'>F&oslash;lgeseddel $x</a></td></tr>";
 }
 print "</tbody></table></td>";
 print "</tbody></table>";
