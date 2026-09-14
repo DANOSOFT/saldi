2f8305cdd993e527bf33d3707f352989ea07d674
Lui Andie Hedlund <luihedlund@gmail.com>
2026-09-14 10:41:41 +0200

    SST-789: render every selected invoice into an ordered PDF batch

--- includes/formfunk.php
+++ includes/formfunk.php
@@ -61,6 +61,7 @@
 // 20260820 Sawaneh Supplier order print totals now match the printed line sums and the
 //                  booked amounts: sum of rounded line sums, VAT on the total (1-3 oere diff).
 //                  Supplier orders no longer print VAT-inclusive prices (customer setting).
+// 20260914 CDX/LH SST-789: Pass the ordered non-email print batch to PDF conversion.
 
 #use PHPMailer\PHPMailer\PHPMailer;
 #use PHPMailer\PHPMailer\Exception; 
@@ -1144,7 +1145,11 @@ if (!function_exists('formularprint')) {
 			#		$psfp1=fopen("$mappe/$printfilnavn.ps","w");
 			#		$htmfp1=fopen("$mappe/$printfilnavn.htm","w");
 		}
+		$printBatchName = $printfilnavn ?? 'udskrift';
+		$printBatchDocuments = array();
+		unset($_SESSION['printBatch']);
 		for ($o = 0; $o < $ordre_antal; $o++) {
+			$printBatchIndex = null;
 			#		$psfp=$psfp1;
 			#		$htmfp=$htmfp1;
 			$ordre_id[$o] *= 1;
@@ -1663,6 +1668,8 @@ if (!function_exists('formularprint')) {
 				$pfnavn = $mappe . "/" . $printfilnavn;
 				$psfp = fopen("$pfnavn.ps", "w");
 				$htmfp = fopen("$pfnavn.htm", "w");
+				$printBatchDocuments[] = array('name' => $printfilnavn, 'background' => $background_pdf_path ?? '');
+				$printBatchIndex = count($printBatchDocuments) - 1;
 				#		} else {
 				#			fclose ($htmfp);
 				#			$pfnavn=$mappe."/".$printfilnavn.".".$ordrenr;
@@ -2235,14 +2242,18 @@ if (!function_exists('formularprint')) {
 				$sum = dkdecimal($sum, 2);
 			}
 
-			if ($id)
+			if ($id) {
 				find_form_tekst($id, 'S', $formular, 0, $linjeafstand, ""); # Sum paa sidste side.
+			}
 
-				if ($ordre_id[$o])
-					bundtekst($ordre_id[$o]); # Uden denne skrives kun  side 1
-					#		if ($mail_fakt) fclose($psfp2);
-					fclose($psfp);
-				fclose($htmfp);
+			if ($ordre_id[$o]) {
+				bundtekst($ordre_id[$o]); # Uden denne skrives kun side 1
+			}
+			fclose($psfp);
+			fclose($htmfp);
+			if ($printBatchIndex !== null) {
+				$printBatchDocuments[$printBatchIndex]['pages'] = max(1, (int)$side - 1);
+			}
 		}
 		// UDSKRIVNING
 		if ($mailantal > 0) {
@@ -2291,6 +2302,13 @@ if (!function_exists('formularprint')) {
 		} elseif ($nomailantal > 0) {
 			print "<big><b>Vent - Udskrift genereres</b></big><br>";
 			$mappe = str_replace('../temp/', '', $mappe);
+			if (count($printBatchDocuments) > 1) {
+				$printfilnavn = $printBatchName . '-batch';
+				$_SESSION['printBatch'] = array(
+					'file' => $mappe . '/' . $printfilnavn,
+					'documents' => $printBatchDocuments
+				);
+			}
 			// 20260807 MJ urlencode returside saa search-parametre ikke laekaer som separate GET-parametre i udskriv.php
 			// 20260812 MJ Brug is_string-guard saa array-input ikke giver PHP-advarsel (null/array -> tom streng)
 			print "<meta http-equiv=\"refresh\" content=\"0;URL=../includes/udskriv.php?locat=$locat&ps_fil=$mappe/$printfilnavn&amp;id=$id&amp;udskriv_til=$udskriv_til&amp;art=$art&amp;bgr=" . urlencode($background_pdf_path) . "&returside=" . urlencode(is_string($returside ?? null) ? $returside : '') . "\">";
