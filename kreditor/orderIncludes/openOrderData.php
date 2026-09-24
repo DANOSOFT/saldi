143adf3ef9d42ee46102c810663a97d200876843
Saul Zayn <zaynsaul@gmail.com>
2026-09-24 07:09:41 +0000

    MB-36: left-join grupper in the has_expiry_items check so has_due_date items without a current-year VG row aren't dropped (CodeRabbit, PR #608)

--- kreditor/orderIncludes/openOrderData.php
+++ kreditor/orderIncludes/openOrderData.php
@@ -4,7 +4,7 @@
 //               \__ \/ _ \| |_| |) | | _ | |) |  <
 //               |___/_/ \_|___|___/|_||_||___/|_\_\
 //
-// --- kreditor/orderIncludes/openOrderData.php --- patch 5.0.0 --- 2026-09-23 ---
+// --- kreditor/orderIncludes/openOrderData.php --- patch 5.0.0 --- 2026-09-24 ---
 // LICENSE
 //
 // This program is free software. You can redistribute it and / or
@@ -37,6 +37,9 @@
 //                 box9 (CodeRabbit, PR #608): an item flagged has_due_date under a group without
 //                 box9='on' was skipping the whole batch input block and saving NULL over any
 //                 existing batch data.
+// 20260924 CL/SZ Left-join grupper instead of inner-joining it (CodeRabbit, PR #608): an item
+//                 with has_due_date=TRUE but no current-year VG group row was still being
+//                 dropped by the join before the has_due_date check ran.
 
 /*
 $attachId    = null;
@@ -322,7 +325,7 @@ if ($omlev) print "<td title ='".findtekst(1512, $sprog_id)."'>O/B</td>";
 // (grupper.box9 = 'on' for art='VG'). Query DB directly since line data is loaded after this header.
 $has_expiry_items = false;
 if ($id) {
-	$_eq = db_select("SELECT ol.vare_id FROM ordrelinjer ol JOIN varer v ON v.id = ol.vare_id JOIN grupper g ON g.kodenr = v.gruppe AND g.art = 'VG' AND g.fiscal_year = '$regnaar' WHERE ol.ordre_id = '$id' AND (g.box9 = 'on' OR v.has_due_date = TRUE) LIMIT 1", __FILE__ . " linje " . __LINE__);
+	$_eq = db_select("SELECT ol.vare_id FROM ordrelinjer ol JOIN varer v ON v.id = ol.vare_id LEFT JOIN grupper g ON g.kodenr = v.gruppe AND g.art = 'VG' AND g.fiscal_year = '$regnaar' WHERE ol.ordre_id = '$id' AND (g.box9 = 'on' OR v.has_due_date = TRUE) LIMIT 1", __FILE__ . " linje " . __LINE__);
 	if (db_fetch_array($_eq)) $has_expiry_items = true;
 }
 if ($has_expiry_items) {
